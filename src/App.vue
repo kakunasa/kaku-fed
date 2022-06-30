42ecf61fcf26666b46184cd9741f2e20f9272b71
System Administrator <root@weicandeMacBook-Pro.local>
2022-07-01 06:21:53 +0900

    second commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img style="height:250px;weight:250px" alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="kakunasa の site" />
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    },
+  };
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
